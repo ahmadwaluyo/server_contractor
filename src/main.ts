b663ce533d9a866f4398b8199083c514b961fa8d
waluyo <ahmad.umb15@gmail.com>
2022-07-09 22:11:19 +0700

    handle cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((req: any, res: any, next: any) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
     allowedHeaders: "*",
     origin: "*",
   });
+  
   await app.listen(3000);
 }
 bootstrap();
